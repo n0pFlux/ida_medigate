68196af9430dfe20c48db0098776ec811c3cd7ec
n0p <0x90@n0p.cc>
2022-07-24 21:35:45 +0200

    Changed the return values in examples/a.cpp from int to void, to stop Visual Studio from complaining. Compiled examples/a.cpp for Windows in different configurations.

--- examples/a.cpp
+++ examples/a.cpp
@@ -5,27 +5,27 @@ using namespace std;
 class A{
 	public:
 	int x_a;
-	virtual int f_a()=0;
+	virtual void f_a()=0;
 };
 
 class B : public A{
 	public:
 	int x_b;
-	int f_a(){x_a = 0;}
-	virtual int f_b(){this->f_a();}
+	void f_a(){x_a = 0;}
+	virtual void f_b(){this->f_a();}
 };
 class Z{
 	public:
-	virtual int f_z1(){cout << "f_z1";}
-	virtual int f_z2(){cout << "f_z2";}
+	virtual void f_z1(){cout << "f_z1";}
+	virtual void f_z2(){cout << "f_z2";}
 };
 
 class C: public B, public Z{
 	public:
-    int f_a(){x_a = 5;}
+    void f_a(){x_a = 5;}
 	int x_c;
-	int f_c(){x_c = 0;}
-	virtual int f_z1(){cout << "f_z3";}
+	void f_c(){x_c = 0;}
+	virtual void f_z1(){cout << "f_z3";}
 };
 
 
